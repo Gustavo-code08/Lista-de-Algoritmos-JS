cfff682704b42989f3b7ff1be88ef3ae584b9dfa
Gustavo-code08 <gustavooj08@gmail.com>
2026-06-16 14:02:22 -0300

    JS-1 ao 10

---
+++ exercicio03.js
@@ -0,0 +1,6 @@
+let largura = Number(prompt("Digite a largura da sala (em metros): "));
+let comprimento = Number(prompt("Digite o comprimento da sala (em metros): "));
+
+const area = largura * comprimento
+
+console.log (`A sala tem ${area} metros quadrados`)
