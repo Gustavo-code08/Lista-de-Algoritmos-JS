4a0c759fb1db140b6666c7fc4f3d1887b911e104
Gustavo-code08 <gustavooj08@gmail.com>
2026-06-16 13:56:53 -0300

    JS-1 ao10

---
+++ exercicio02.js
@@ -0,0 +1,7 @@
+let anoatual = Number(prompt("Digite o ano atual: "));
+let anonas = Number(prompt("Digite seu ano de nascimento: "));
+
+const idade = anoatual - anonas;
+
+console.log(`Sua idade é aproximadamente ${idade} anos`)
+alert(`Sua idade é aproximadamente ${idade} anos`)
